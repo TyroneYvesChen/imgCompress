598136cdd91375ae045536b1bd7a6242be726913
TyroneYvesChen <243307272@qq.com>
2017-06-25 23:30:07 +0800

    imgCompress OI

---
+++ imgCompress.js
@@ -0,0 +1,13 @@
+/**
+ * @imgCompress
+ * @author Tyrone Yves Chen
+ * @version 1.0
+ * @description 图片压缩小工具
+ * @disclaimer 我只是代码的搬运工，借鉴于网上各位大佬的代码修改后在项目中使用，如有雷同，纯属巧合
+ * @PS：汤圆萌萌哒~
+ * 感谢jzz大佬和小杰果果大佬的代码给我提供了思路
+ * jzz大佬GitHub地址：https://github.com/jzz2649
+ * Param ：
+ * options = {
+    };
+ */
