49b90b8e7934ec53e718627be02a9562f2f4c18d
CZY_1992 <243307272@qq.com>
2017-06-26 15:53:59 +0800

    喵的让我缓缓，头晕

--- imgCompress.js
+++ imgCompress.js
@@ -17,33 +17,120 @@
             var ImgCompress = function (element, options) {
                 if (!this.validateElement(element)) return;
                 this.optionsHandle(options);
-                this._this = this;
                 this.init(element);
             };
 
             var imgCompress = ImgCompress.prototype;
-
+            //初始化
             imgCompress.init = function (element) {
                     this.eventChange(element);
             };
-
+            //默认参数
             imgCompress.defaults = {
                 isCompress: false,
                 sizeConstrained: 10240,
                 scale: 0.6,
                 onSize: 500,
                 w_scale: 640,
-                callback: function () {}
+                callbackFn: function () {}
             };
-
+            //input的chang事件
             imgCompress.eventChange = function (element) {
+                var _this = this,
+                    params = _this.params;
                 element.onchange = function (e) {
                     console.log(e);
-                    var files = element.files[0];
+                    var files = e.currentTarget.files[0];              //传入文件详情
+                    var filename = e.currentTarget.files[0].name;		//文件名
+                    var byteSize = e.currentTarget.files[0].size;		//文件大小
+                    var lastname = filename.substring(filename.lastIndexOf(".") + 1, filename.length);
                     console.log(files);
+                    console.log(element.files[0]);
+                    if (params.isCompress){
+                        _this.imgCompressHandle(files);
+                    }else {
+                        _this.validateFileReader(files);
+                        params.callbackFn();
+                    }
                 }
             };
 
+            //图片压缩
+            imgCompress.imgCompressHandle = function (files, callback) {
+                var size = file.size;
+                var params = this.params;
+                var type = params.type || file.type;
+                var canvas = document.createElement('canvas');
+                var ctx = canvas.getContext('2d');
+                var ObjectURL = null;
+                var img = new Image();
+                img.src = base64;
+                img.onload = function () {
+                    var width = img.width;
+                    var height = img.height;
+
+                    if (width > params.w_scale) {
+                        canvas.width = params.w_scale;
+                        canvas.height = height * params.w_scale / width;
+                    } else {
+                        canvas.width = width;
+                        canvas.height = height;
+                    }
+
+                    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+                    var dataURL;
+                    if (size > params.onSize * 1024) {
+                        dataURL = canvas.toDataURL(type, params.scale);
+                    } else {
+                        dataURL = canvas.toDataURL(type);
+                    }
+
+                    callback(dataURL);
+                    img = null;
+                    ObjectURL && URL.revokeObjectURL(ObjectURL);
+                };
+            };
+
+            //FileReader判断,获取base64
+            imgCompress.validateFileReader = function (files) {
+                var _this = this,
+                    ObjectURL = null,
+                    base64 = "";
+                if (FileReader) {
+                    var reader = new FileReader();
+                    reader.readAsDataURL(files);
+                    reader.onload = function(e) {
+                        base64 = e.target.result;
+                        _this.imgCompressHandle(base64)
+                    };
+                } else {
+                    ObjectURL = URL.createObjectURL(file);
+                    _this.imgCompressHandle(ObjectURL);
+                }
+                return base64;
+            };
+
+            //dom判断
+            imgCompress.validateElement = function (element) {
+                var boolean = true;
+                try {
+                    if (element === undefined || element === null) {
+                        boolean = false;
+                        throw new Error("element is not found");
+                    }
+                    if (typeof element !== 'object') {
+                        boolean = false;
+                        throw new Error("element is not dom");
+                    }
+                }
+                catch (err){
+                    console.log(err);
+                }
+                return boolean;
+            };
+
+            //参数处理
             imgCompress.optionsHandle = function (options) {
                 var params = {},
                     options = options || {},
@@ -63,42 +150,6 @@
                     }
                 }
                 this.params = params;
-            };
-
-            imgCompress.imgCompressHandle = function () {
-
-            };
-
-            imgCompress.validateFileReader = function (files) {
-                if (FileReader) {
-                    var reader = new FileReader();
-                    reader.readAsDataURL(files);
-                    reader.onload = function(e) {
-                        var base64 = e.target.result;
-                        handle(base64)
-                    };
-                } else {
-                    ObjectURL = URL.createObjectURL(file);
-                    handle(ObjectURL);
-                }
-            };
-
-            imgCompress.validateElement = function (element) {
-                var boolean = true;
-                try {
-                    if (element === undefined || element === null) {
-                        boolean = false;
-                        throw new Error("element is not found");
-                    }
-                    if (typeof element !== 'object') {
-                        boolean = false;
-                        throw new Error("element is not dom");
-                    }
-                }
-                catch (err){
-                    console.log(err);
-                }
-                return boolean;
             };
 
             window.ImgCompress = ImgCompress;
